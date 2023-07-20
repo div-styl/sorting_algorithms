dbb37ce72abb5ccc463c92e9c122dcf87b2e1a7d
div-styl <gouhmada@gmail.com>
2023-07-20 13:08:26 +0100

    sorted list is done

--- sort.h
+++ sort.h
@@ -29,4 +29,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 /*sorting prototypes*/
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
 #endif
