bf182ea4e5e482973d29ef3cddd2d7fa063c4fd1
div-styl <gouhmada@gmail.com>
2023-07-23 11:01:40 +0100

    betty task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,11 +8,11 @@
 */
 void swap1(int *x, int *y)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *x;
-    *x = *y;
-    *y = tmp;
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
 }
 /**
  * get_first_min - get the first minimum value
@@ -22,22 +22,23 @@ void swap1(int *x, int *y)
 */
 void get_first_min(int array[], size_t size)
 {
-    size_t i = 1;
-    int min = array[0];
-    size_t minIndex = 0;
-    while (i < size)
-    {
-        if (array[i] < min)
-        {
-            min = array[i];
-            minIndex = i;
-        }
-        i++;
-    }
-    if (minIndex != 0)
-    {
-        swap1(&array[0], &array[minIndex]);
-    }
+	size_t i = 1;
+	int min = array[0];
+	size_t minIndex = 0;
+
+	while (i < size)
+	{
+		if (array[i] < min)
+		{
+			min = array[i];
+			minIndex = i;
+		}
+		i++;
+	}
+	if (minIndex != 0)
+	{
+		swap1(&array[0], &array[minIndex]);
+	}
 }
 
 /**
@@ -48,23 +49,23 @@ void get_first_min(int array[], size_t size)
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    get_first_min(array, size);
+	get_first_min(array, size);
 
-    for (i = 1; i < size - 1; i++)
-    {
-        size_t j;
-        size_t minIndex = i;
+	for (i = 1; i < size - 1; i++)
+	{
+		size_t j;
+		size_t minIndex = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[minIndex])
-            {
-                minIndex = j;
-            }
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[minIndex])
+			{
+				minIndex = j;
+			}
+		}
 
-        swap1(&array[i], &array[minIndex]);
-    }
+		swap1(&array[i], &array[minIndex]);
+	}
 }
