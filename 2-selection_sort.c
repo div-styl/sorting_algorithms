17bab1eefe149f3b68e384c4df032399389fb939
adil_ma <itsadilhadid@gmail.com>
2023-07-23 15:49:05 +0100

    ALX higher level

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,7 +5,7 @@
  * @x: first integer
  * @y: second integer
  * Return: void
-*/
+ */
 void swap1(int *x, int *y)
 {
     int tmp;
@@ -14,46 +14,21 @@ void swap1(int *x, int *y)
     *x = *y;
     *y = tmp;
 }
-/**
- * get_first_min - get the first minimum value
- * @array: array
- * @size: size of array
- * Return: void
-*/
-void get_first_min(int array[], size_t size)
-{
-    size_t i = 1;
-    int min = array[0];
-    size_t minIndex = 0;
-    while (i < size)
-    {
-        if (array[i] < min)
-        {
-            min = array[i];
-            minIndex = i;
-        }
-        i++;
-    }
-    if (minIndex != 0)
-    {
-        swap1(&array[0], &array[minIndex]);
-    }
-}
 
 /**
  * selection_sort - sort an array of integers in ascending order
  * @array: array
  * @size: size of array
  * Return: void
-*/
+ */
 void selection_sort(int *array, size_t size)
 {
+
     size_t i;
 
-    get_first_min(array, size);
-
-    for (i = 1; i < size - 1; i++)
+    for (i = 0; i < size - 1; i++)
     {
+
         size_t j;
         size_t minIndex = i;
 
@@ -66,5 +41,7 @@ void selection_sort(int *array, size_t size)
         }
 
         swap1(&array[i], &array[minIndex]);
+
+        print_array(array, size);
     }
 }
