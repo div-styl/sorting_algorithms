83c81c1e720ad0a3768a8f27400f858f7c407638
adil_ma <itsadilhadid@gmail.com>
2023-07-23 16:49:00 +0100

    fixed task 3 quick sort with lumoto partition

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -4,7 +4,7 @@
  * @x: first integer
  * @y: second integer
  * Return: void
-*/
+ */
 void swap2(int *x, int *y)
 {
 	int tmp = *x;
@@ -13,34 +13,41 @@ void swap2(int *x, int *y)
 }
 
 /**
- * partition - partition the array
+ * lomuto_partition - partition the array
  * @array: array to be sorted
  * @low: lowest index of the array
  * @high: highest index of the array
  * @size: size of the array
  * Return: index of the pivot
-*/
-int partition(int array[], int low, int high, size_t size)
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
 	int i = low;
 	int j;
 
+	(void)size;
+
 	for (j = low; j < high; j++)
 	{
 		if (array[j] <= pivot)
 		{
-			swap2(&array[j], &array[i]);
+			if (i != j)
+			{
+				swap2(&array[j], &array[i]);
+				print_array(array, size);
+			}
 			i++;
 		}
 	}
-	swap2(&array[i], &array[high]);
+	if (i != high)
+	{
+		swap2(&array[i], &array[high]);
+		print_array(array, size);
+	}
 
-	print_array(array, size);
-
-	return (i);
+	return i;
 }
-
 /**
  * quickSort - quick sort algorithm
  * @array: array to be sorted
@@ -48,27 +55,31 @@ int partition(int array[], int low, int high, size_t size)
  * @high: highest index of the array
  * @size: size of the array
  * Return: void
-*/
+ */
 void quickSort(int array[], int low, int high, size_t size)
 {
 	if (low < high)
 	{
-		int center = partition(array, low, high, size);
+		int center = lomuto_partition(array, low, high, size);
 
 		quickSort(array, low, center - 1, size);
 		quickSort(array, center + 1, high, size);
 	}
 }
+
 /**
  * quick_sort - quick sort algorithm
  * @array: array to be sorted
  * @size: size of the array
  * Return: void
-*/
+ */
 void quick_sort(int *array, size_t size)
 {
 	int low = 0;
 	int high = size - 1;
+
+	if (!array || size < 2)
+		return;
 
 	quickSort(array, low, high, size);
 }
